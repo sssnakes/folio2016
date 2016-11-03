199c657bc53638960cc598364325f8d5ba1f946c
sssnakes <finnan.barry@gmail.com>
2016-11-03 13:37:12 +0100

    dual scrolling implemented

--- js/app.js
+++ js/app.js
@@ -30,17 +30,28 @@ $(document).ready(function() {
 
   });
 
-  $('#left-block').scroll(function(){
+  // $('#left-block').scroll(function(){
+  //
+  //   var wH = $(window).height(),
+  //       tA = ($('#left-box').height() - wH) / 100,
+  //       sA = (-($('#left-box').offset().top)) / tA;
+  //       tB = ($('.image-list').height() - wH) / 100,
+  //       sB = (-($('.image-list').offset().top)) / tB;
+  //
+  //   $('.image-list').animate({
+  //     scrollTop: +10
+  //   }, 10);
+  //
+  // });
 
-    var wH = $(window).height();
-    var hA = $('#left-block').height() - wH;
-    var hB = $('#right-block').height() - wH;
-
-    console.log(hA);
-    console.log(hB);
-
-  });
-
+  var $divs = $('#left-block, #right-block');
+  var sync = function(e){
+      var $other = $divs.not(this).off('scroll'), other = $other.get(0);
+      var percentage = this.scrollTop / (this.scrollHeight - this.offsetHeight);
+      other.scrollTop = percentage * (other.scrollHeight - other.offsetHeight);
+      setTimeout( function(){ $other.on('scroll', sync ); },1);
+  }
+  $divs.on( 'scroll', sync);
 
 
 });
