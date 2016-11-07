f90b5855be7c96da11a51a43c88405c197abbc3d
sssnakes <finnan.barry@gmail.com>
2016-11-07 16:20:28 +0100

    scrolling reworked, still only functions after first scroll

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
 
   // $divs.customScrollbar();
-  $('#left-block').customScrollbar();
+  // $('#left-block').customScrollbar();
 
-  var $divs = $('#left-block, #right-block');
+  var $divs = $('#right-block, #left-block');
   var sync = function(e){
       var $other = $divs.not(this).off('scroll'), other = $other.get(0);
       var percentage = this.scrollTop / (this.scrollHeight - this.offsetHeight);
       other.scrollTop = percentage * (other.scrollHeight - other.offsetHeight);
+      // setTimeout( function(){ $other.on('scroll', sync ); }, 10);
+      // $other.on('scroll', sync );
   }
   $divs.on( 'scroll', sync);
+
+  // $(window).focus(function() {
+  //   $('.tabber-image').fadeOut(500);
+  // });
+  //
+  // $(window).blur(function() {
+  //   $('.tabber-image').fadeIn(200);
+  // });
 
 });
